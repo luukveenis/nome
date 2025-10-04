95c13698e1e2aa40e0ca91f48dbb1b98a411d55d
Luuk Veenis <luuk@corellia.localdomain>
2025-10-03 17:59:27 -0700

    Don't install system packages for now
    Should be able to add these with home manager.

--- flake.nix
+++ flake.nix
@@ -14,9 +14,9 @@
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages = [
-	pkgs.vim
-      ];
+      # environment.systemPackages = [
+      # pkgs.vim
+      # ];
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
