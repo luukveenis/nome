83e2d012f08543235af92ebd3565a9c21f50b8f9
Luuk Veenis <luukveenis@gmail.com>
2025-11-13 13:06:56 -0800

    Update tmux bindings
    customPaneNavigationAndResize makes hjkl work correctly in vim mode.

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -7,6 +7,7 @@
   historyLimit = 10000;
   keyMode = "vi";
   prefix = "C-Space";
+  customPaneNavigationAndResize = true;
 
   plugins = with pkgs; [
     tmuxPlugins.catppuccin
@@ -15,5 +16,23 @@
 
   extraConfig = ''
     set -g @catppuccin_flavour 'macchiato'
+
+    # Fix default dir on new window/pane
+    bind '"' split-window -c "#{pane_current_path}"
+    bind % split-window -h -c "#{pane_current_path}"
+    bind c new-window -c "#{pane_current_path}"
+
+    # Arrow keys to reorder windows
+    bind -n C-Left swap-window -t -1
+    bind -n C-Right swap-window -t +1
+
+    # Make arrow keys more useful
+    bind -n S-Up copy-mode
+    bind -n S-Down command-prompt
+    bind -n S-Right next-window
+    bind -n S-Left previous-window
+
+    # Restore clear screen as <prefix> C-a
+    bind C-l send-keys 'C-l'
   '';
 }
